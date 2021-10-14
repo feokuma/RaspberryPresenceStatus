df25c4722e648c0da8ef32cb326f06a57cec742b
Fernando Okuma <feokuma@gmail.com>
2021-10-14 01:41:03 -0300

    :test_tube: Add tests of StatusLedController

--- test/RaspberryPresenceStatus.Unit/Controllers/StatusLedControllerTests.cs
+++ test/RaspberryPresenceStatus.Unit/Controllers/StatusLedControllerTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Autofac.Extras.FakeItEasy;
 using FakeItEasy;
@@ -10,17 +11,45 @@ namespace RaspberryPresenceStatus.Unit.Controllers
 {
     public class StatusLedControllerTests
     {
-        [Test]
-        public async Task PutShouldCallDrawAvaliableOfDisplayService()
+        [TestCase(PresenceStatusEnum.Avaliable)]
+        [TestCase(PresenceStatusEnum.Busy)]
+        [TestCase(PresenceStatusEnum.DoNotDisturb)]
+        [TestCase(PresenceStatusEnum.Away)]
+        [TestCase(PresenceStatusEnum.Offline)]
+        public void DrawPresenceStatusShouldGetStatusImageFromEnumWithStatusReceived(PresenceStatusEnum statusEnum)
         {
             using var fake = new AutoFake();
-            var status = PresenceStatusEnum.Avaliable;
             var controller = fake.Resolve<StatusLedController>();
 
-            await controller.Put(status);
+            controller.DrawPresenceStatus(statusEnum);
 
-            A.CallTo(() => fake.Resolve<IDisplayService>().DrawStatus(status))
+            A.CallTo(() => fake.Resolve<IMicrosoftTeamsStatusImages>().StatusImageFromEnum(statusEnum))
                 .MustHaveHappenedOnceExactly();
+        }
+
+        [Test]
+        public void DrawPresenceStatusShouldCallDrawBytesFromDisplayServiceWithImageBytesFromIMicrosoftTeamsStatusImages()
+        {
+            using var fake = new AutoFake();
+            var controller = fake.Resolve<StatusLedController>();
+            var fakeImageBytes = new byte[] { 0x00, 0x01 };
+            A.CallTo(() => fake.Resolve<IMicrosoftTeamsStatusImages>().StatusImageFromEnum(A<PresenceStatusEnum>._))
+                .Returns(fakeImageBytes);
+
+            controller.DrawPresenceStatus(GetRandomPresenceEnumStatus());
+
+            A.CallTo(() => fake.Resolve<IDisplayService>().DrawBytes(fakeImageBytes))
+                .MustHaveHappenedOnceExactly();
+        }
+
+
+
+        private static PresenceStatusEnum GetRandomPresenceEnumStatus()
+        {
+            var random = new Random();
+            var values = typeof(PresenceStatusEnum).GetEnumValues();
+
+            return (PresenceStatusEnum)random.Next(values.Length);
         }
     }
 }
