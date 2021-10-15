0cab64da4d8dd8f7595dea93cad3cdb439799b2f
Fernando Okuma <feokuma@gmail.com>
2021-10-15 00:34:51 -0300

    :recycle: Refactory code

--- src/Services/MicrosoftTeamsStatusImages.cs
+++ src/Services/MicrosoftTeamsStatusImages.cs
@@ -1,12 +1,13 @@
+using Iot.Device.Graphics;
 using RaspberryPresenceStatus.Models.Enuns;
 
 namespace RaspberryPresenceStatus.Services
 {
-    public class MicrosoftTeamsStatusImages : IMicrosoftTeamsStatusImages
-    {
-        public byte[] StatusImageFromEnum(PresenceStatusEnum statusEnum)
-        {
-            throw new System.NotImplementedException();
-        }
-    }
+	public class MicrosoftTeamsStatusImages : IMicrosoftTeamsStatusImages
+	{
+		public byte[] StatusImageFromEnum(PresenceStatusEnum statusEnum)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
 }
